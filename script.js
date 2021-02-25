6eda0ccfd151e0fdfac812ba39383a030bffb84b
johanneslepsius <johannes.lepsius@chasacademy.se>
2021-02-25 21:54:36 +0100

    adds create card functionality

--- script.js
+++ script.js
@@ -4,6 +4,18 @@ $(() => {
     $("#review").toggle("show", "linear")
   });
 
+  function opendialog() {
+    $( ".todo" ).on("click", function(){
+    $(".dialog").dialog("close");
+    let dialog = $(this).data('id');
+    $( dialog ).dialog( "open" );
+    // finns ingen bra lösning med typ children eller find?? slippa hårdkoda id och data-id
+  });
+};
+
+opendialog();
+
+
   $("form").submit( function(event) {
       event.preventDefault();
       let todos = $(".todo");
@@ -15,6 +27,7 @@ $(() => {
       const deadline = $("input[name=deadline]").val();
       let appendtodo = `
           <div id="todo${todos +1}" class="todo" data-id="#dialog${todos +1}">
+          <p>click or drag me</p>
               <div class="dialog" id="dialog${todos +1}">
                   <div class="tabs">
                       <ul>
@@ -36,53 +49,13 @@ $(() => {
       //     dateFormat: "yy-mm-dd"
       //     });
           
-      // $("#todo4 .deadline").val(deadline);
-      // // $("#backlog").append( blah );
-      // console.log(deadline);
-
-      $( `#todo${todos +1}` ).dialog({
+      $(`#todo${todos +1} .deadline`).val(deadline);
+      
+      $( `#dialog${todos +1}` ).dialog({
         autoOpen: false
       });
+      opendialog();
   });
-
-  // const createParams = new URLSearchParams(window.location.search);
-
-  // if(createParams.get("info 1") !== null){
-  //     let todos = $(".todo");
-  //     todos = todos.length;
-  //     // console.log(todos.length);
-  //     const info1 = createParams.get("info 1");
-  //     const info2 = createParams.get("info 2");
-  //     const info3 = createParams.get("info 3");
-  //     const deadline = createParams.get("deadline");
-  //     // let date = deadline.replace(/-/g, "/");
-  //     // let date = $.datepicker.parseDate("yy-mm-dd", deadline);
-  //     let appendtodo = `
-  //         <div id="todo${todos +1}" class="todo" data-id="#dialog${todos +1}">
-  //             <div class="dialog" id="dialog${todos +1}">
-  //                 <div class="tabs">
-  //                     <ul>
-  //                         <li><a href="#fragment-1.${todos +1}">info 1</a></li>
-  //                         <li><a href="#fragment-2.${todos +1}">info 2</a></li>
-  //                         <li><a href="#fragment-3.${todos +1}">info 3</a></li>
-  //                     </ul>
-  //                     <div id="fragment-1.${todos +1}">${info1}</div>
-  //                     <div id="fragment-2.${todos +1}">${info2}</div>
-  //                     <div id="fragment-3.${todos +1}">${info3}</div>
-  //                 </div>
-  //                 <p>Deadline:</p><input type="text" class="deadline"></input>
-  //             </div>
-  //         </div>`
-  //     $("#backlog").append( appendtodo );
-  //     $(".deadline").datepicker({
-  //         minDate: 0,
-  //         dateFormat: "yy-mm-dd"
-  //         });
-          
-  //     $("#todo4 .deadline").val(deadline);
-  //     // $("#backlog").append( blah );
-  //     console.log(deadline);
-  // };
   $( ".todo" )
     .draggable({snap: ".snapcontainer", snapMode: "inner"})
     // why is this shit fucking up the draggable????            toggle class   css scale
@@ -107,16 +80,7 @@ $(() => {
     autoOpen: false
   });
 
-  $( ".todo" ).on("click", function(){
-    
-    console.log($(this))
-    // $(this).parent().find(".dialog").dialog( "open" )
-
-    $(".dialog").dialog("close");
-    let dialog = $(this).data('id');
-    $( dialog ).dialog( "open" );
-    // finns ingen bra lösning med typ children eller find?? slippa hårdkoda id och data-id
-  });
+  
   $(".tabs").tabs();
 
   $(".deadline").datepicker({
